03e8fa5a85ea792952067389c01f4f795060c090
RB <rbungard@cs.cmu.edu>
2016-11-16 13:34:40 -0500

    Have graph load input JSON file content

--- cytoscape/scripts/test.js
+++ cytoscape/scripts/test.js
@@ -1,18 +1,31 @@
-var edges = {}
-
 window.addEventListener("load", function(){
-
+    var initial_nodes = [];
+    /*
+    $.ajax({
+        type: "GET",
+        url: "input/nodes.json",
+        dataType: "json",
+        async: false,
+        success: function (data) { 
+            initial_nodes = data; 
+        }})
+        .fail(function(xhr, status, error) {
+            var err = status + ", " + error;
+            console.log("Request failed: " + err)
+    });
+    var column_count = Math.floor(Math.sqrt(initial_nodes.length));
+    */
+   var column_count = 3;
+    
     var cy = window.cy = cytoscape({
         container: document.getElementById('cy'),
 
-        ready: function(){
-        },
+        ready: function(){ },
 
         //RB 11/15/16: This should be recalculated dynamically based on the nodes.
         layout: {
             name: 'grid',
-            rows: 4,
-            cols: 2
+            cols: column_count
         },
 
         style: [
@@ -82,23 +95,14 @@ window.addEventListener("load", function(){
 
         //RB 11/15/16: Nodes should come from input. No starting edges.
         elements: {
-            nodes: [
-                { data: { id: 'node-1', name: 'Support exploratory search and discovery of "who is doing X at CMU"' } },
-                { data: { id: 'node-2', name: 'Construct a map of the problems that people are working on' } },
-                { data: { id: 'node-3', name: 'Re-represent each paper as a problem graph' } },
-                { data: { id: 'node-4', name: 'Extract them from the paper' } },
-                { data: { id: 'node-5', name: 'Specify relationships between problem nodes' } },
-                { data: { id: 'node-6', name: 'Distributed approach: infer hierarchy from local "path" questions' } },
-                { data: { id: 'node-7', name: 'Ensure accuracy' } },
-                { data: { id: 'node-8', name: 'Model and account for noise rates' } },
-                { data: { id: 'node-9', name: 'Understand workers\' error rates' } }
+            nodes: //initial_nodes,
+            [
+                { data: { id: 'j', name: 'Jerry' } },
+                { data: { id: 'e', name: 'Elaine' } },
+                { data: { id: 'k', name: 'Kramer' } },
+                { data: { id: 'g', name: 'George' } }
             ],
-            edges: [
-                /*
-                { data: { source: 'node-1', target: 'node-2' } },
-                { data: { source: 'node-2', target: 'node-3' } },
-                */             
-            ]
+            edges: []
         },
     });
 
